9cfd742de7cd5ecee75b9ab0cdf880d7b44e2c73
SavionCychet <enochcychet@gmail.com>
2020-06-25 00:57:07 -0500

    Created introduction

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,6 +6,15 @@ class Bio extends React.Component {
       <div className="bio-section col-4">
         <h1 className="about-me animate__animated animate__bounceInLeft">ABOUT ME</h1>
         <div className="bio animate__animated animate__bounceInLeft">
+          <div className="introduction">
+          <img src={require("../graphic-assets/images/bitmoji.jpg")} class="bitmoji" alt="..." />
+          <h1>
+            Hi, I'm Savion!
+          </h1>
+          <h2>
+          I'm a front-end web developer able to build and maintain interactive web pages with UI effects, animations, dynamic user experiences! 
+          </h2>
+          </div>
         </div>
       </div>
     );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends React.Component {
           <span class="head-text">Savion Devs</span>
           <ul class="nav-buttons">
           <a href="https://linkedin.com/in/savion-amerson-629a32190/" target="blank"><li><img src={require("../graphic-assets/images/linkedin.png")} class="" alt="..." /></li></a>
-          <a href="https://github.com/savioncychet" target="blank"><li><img src={require("../graphic-assets/images/github.png")} class="" alt="..." /></li></a>
+          <a href="https://github.com/savionamerson" target="blank"><li><img src={require("../graphic-assets/images/github.png")} class="" alt="..." /></li></a>
           <div className="resume">
           <a href="Savion Amerson Resume 2020.pdf" download><li><img src={require("../graphic-assets/images/download.png")} class="" alt="..." /></li>
           </a><h1>RESUME</h1>
