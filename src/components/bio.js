c51ef2d9ab174411f5f79142df0f8deeb6553586
SavionCychet <enochcychet@gmail.com>
2020-07-01 16:16:28 -0500

    Added mobile responsivenss

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,7 +3,7 @@ import React from "react";
 class Bio extends React.Component {
   render() {
     return (
-      <div className="bio-section col-4">
+      <div className="bio-section col-md-4 col-sm-6">
         <h1 className="about-me animate__animated animate__bounceInLeft">ABOUT ME</h1>
         <div className="bio animate__animated animate__bounceInLeft">
           <div className="introduction">
